64eba8740dd4dc46eedae60de156963eb746d7f2
Hunterly01 <mhunterly@gmail.com>
2025-08-23 14:54:33 +0100

    calculate safePeriod

--- java/MenstrualApp.java
+++ java/MenstrualApp.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 public class MenstrualApp{
 public static void main (String [] args){
-Scanner input = new Scanner(System.in){
+Scanner input = new Scanner(System.in);
 while(true){
 System.out.println(""" 
 			 WELCOME TO MENSTRUAL CYCLE APP
 			
-			1.Calculate safe periods
-			2.Calculate Ovulation periods
-			
-			
+			1.Calculate safe periods			
 			""");
-int option = input.next();
+int option = input.nextInt();
 switch(option){
+	case 1: System.out.println("Enter your cycle Length:");
+		int userInput = input.nextInt();
+		calculateSafePeriods(userInput);
+		break;
+
 
 }
 }
+}
+public static void calculateSafePeriods(int CycleLength){
+int firstSafePeriod = 7;
+int secondSafePeriodStart = CycleLength - firstSafePeriod;
+int secondSafePeriodEnd = CycleLength;
+System.out.println("first safe period: Day 1 to " + firstSafePeriod);
+System.out.println("second safe period: Day " + secondSafePeriodStart + " to " + secondSafePeriodEnd);
+
+}
+}
+
 

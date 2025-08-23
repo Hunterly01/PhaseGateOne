b6a25fa1b9991c1271af4ed74a13babdf44528aa
Hunterly01 <mhunterly@gmail.com>
2025-08-23 14:22:40 +0100

    java first try

---
+++ java/MenstrualApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MenstrualApp{
+public static void main (String [] args){
+Scanner input = new Scanner(System.in){
+while(true){
+System.out.println(""" 
+			 WELCOME TO MENSTRUAL CYCLE APP
+			
+			1.Calculate safe periods
+			2.Calculate Ovulation periods
+			
+			
+			""");
+int option = input.next();
+switch(option){
+
+}
+}
+
